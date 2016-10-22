e4adb5c7a259ca90e5cafb8e7bbec7c1a79600e5
Calvin Rose <calsrose@gmail.com>
2016-10-21 21:26:04 -0400

    All big things begin small except some big things - Calvin Rose.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var PORT = process.env.PORT || 8000;
+
+var app = express();
+
+app:get('/', function (req, res) {
+    res.send('Hello, Heroku!');
+});
+
+app.listen(PORT, function () {
+    console.log('App started on port ' + port + '...');
+});
