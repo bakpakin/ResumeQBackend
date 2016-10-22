b0f70bff1d206b3aaf56a1de9b6c4c08eceada1f
Calvin Rose <calsrose@gmail.com>
2016-10-21 23:23:17 -0400

    .

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var multer = require('multer');
+var uploads = multer({dest: 'uploads/'});
+var uuid = require ('node-uuid');
 var path = require('path');
 var PORT = process.env.PORT || 8000;
 
@@ -13,8 +15,24 @@ app.get('/', function (req, res) {
 });
 
 // Resume UPLOADS
-app.post('/resume_submit', function (req, res) {
+app.post('/resume_submit', function (req, res, next) {
     console.log('Resume submission received...');
+    return next();
+}, uploads.single('resume'), function (req, res) {
+    // Here the file has been uploaded.
+    console.log(req.body); //form fields
+    console.log(req.file); //form files
+	res.status(204).end();
+});
+
+app.get('/resume/:id', function (req, res) {
+    var id = req.params.id;
+    console.log('Resume "' + id + '" requested.');
+    res.send({
+        id: id,
+        success: true,
+        data: 'LOL'
+    });
 });
 
 app.listen(PORT, function () {
