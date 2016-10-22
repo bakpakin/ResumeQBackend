a939f2542f78292b2aa793cb9eec7ee757337f02
Calvin Rose <calsrose@gmail.com>
2016-10-21 22:25:29 -0400

    .

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ var PORT = process.env.PORT || 8000;
 var app = express();
 
 app.get('/', function (req, res) {
+    console.log('Homepage hit...');
     res.sendFile('./index.html');
+});
+
+// Resume UPLOADS
+app.post('/resume_submit', function (req, res) {
+    console.log('Resume submission received...');
 });
 
 app.listen(PORT, function () {
