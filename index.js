a4e57bb82d5c6e9627ac8394bd7d763863abd05e
Calvin Rose <calsrose@gmail.com>
2016-10-23 06:14:51 -0400

    .

--- index.js
+++ index.js
@@ -53,12 +53,7 @@ app.post('/resume_submit', function (req, res, next) {
 app.post('/resume_submit_mobile', function (req, res, next) {
     console.log('Resume submission received...');
     return next();
-}, uploads.single('resume'), function (req, res) {
-    // Here the file has been uploaded.
-    console.log(req.body); //form fields
-    console.log(req.file); //form files
-    res.send(req.file.filename)
-});
+}, uploads.single('resume'));
 
 app.get('/resume/test.pdf', function (req, res) {
     res.type('application/pdf');
