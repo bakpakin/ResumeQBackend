8bbd4509a9b75814e6d3082714c980e522b271dc
Calvin Rose <calsrose@gmail.com>
2016-10-23 00:52:22 -0400

    .

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ var uuid = require('node-uuid');
 var path = require('path');
 var fs = require('fs');
 var PORT = process.env.PORT || 8000;
-var mkdirp = require('mkdirp');
 var bodyParser = require('body-parser');
 
 var db = new nedb({ filename: './datafile', autoload: true });
 db.loadDatabase();
 var app = express();
 app.use(bodyParser.json());
-
-mkdirp.sync('./collections');
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
